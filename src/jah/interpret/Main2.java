ffde0c84ec52c59b9bdd6c63f7060df446aef854
accelwo <38794741+accelwo@users.noreply.github.com>
2018-07-28 20:44:13 -0300

    Add files via upload

---
+++ src/jah/interpret/Main2.java
@@ -0,0 +1,36 @@
+package jah.interpret;
+
+import jah.parser.*;
+import jah.node.Start;
+import jah.lexer.*;
+import jah.node.*;
+import java.io.*;
+import jah.lexer.*;
+
+class Main2 {
+	public static void main(String[] args) {
+		try {
+			String arquivo = "exemplo.sa";
+			Lexer lexer = 
+					new Lexer(
+							new PushbackReader(
+									new FileReader(arquivo), 1024));
+
+			// Token token;
+			while (true) {
+				try {
+					// token = lexer.next();
+					Parser parser = new Parser(lexer);
+					parser.parse();
+				} catch (LexerException e) {
+					// token = lexer.next();
+					System.out.print(e.getMessage());
+				}
+			}
+		} catch (Exception e) {
+			// TODO: handle exception
+			System.out.println(e.getMessage());
+		}
+
+	}
+}
