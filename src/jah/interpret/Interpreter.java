53fe7ca27d696b4d34e549e79171f69e1c486110
Artur Santos Nascimento <arturxz@outlook.com.br>
2018-08-29 22:37:45 -0300

    semantico funcionando para producao pgr, adicionados metodos de teste de numeros inteiros e de ponto flutuante

--- src/jah/interpret/Interpreter.java
+++ src/jah/interpret/Interpreter.java
@@ -4,19 +4,50 @@ package jah.interpret;
 import jah.node.*;
 import jah.analysis.*;
 import java.lang.System;
+import java.util.Hashtable;
 
 public class Interpreter extends DepthFirstAdapter {
-
-/*
-	public void caseAProgram(AProgram node) {
-		String lhs = node.getLeft().getText().trim();
-		String rhs = node.getRight().getText().trim();
-		int result = (new Integer(lhs)).intValue() + (new Integer(rhs)).intValue();
-		System.out.println(lhs + "+" + rhs + "=" + result);
+	public static Hashtable<Node, Integer> 	ht_int = new Hashtable<Node, Integer>();
+	public static Hashtable<Node, Float> 	ht_flo = new Hashtable<Node, Float>();
+	public static Hashtable<Node, String> 	ht_str = new Hashtable<Node, String>();
+	
+	// PRODUCOES PARA PROGRAMA GERAL
+	public void outASemTudoPgr(ASemTudoPgr node) {
+		System.out.println("Compilando programa: " + node.getId() +" sem declaracoes e comandos.");
 	}
-*/
-	public void soma(ASomaExp node) {
-		String somal = node.getL().toString().trim();
-		System.out.println(somal);
+
+	public void outAComDeclPgr(AComDeclPgr node) {
+		System.out.println("Compilando programa: " + node.getId() +" apenas com declaracoes");
+	}
+
+	public void outAComCommPgr(AComCommPgr node) {
+		System.out.println("Compilando programa: " + node.getId() +" apenas com comandos.");
+	}
+
+	public void outAComTudoPgr(AComTudoPgr node) {
+		System.out.println("Compilando programa: " + node.getId() +" com declaracoes e comandos.");
+	}
+	
+	// DECLARACOES
+	public void caseAVariaveisDecl(AVariaveisDecl node) {
+		
+	}
+	
+	private boolean testaInteiro(String str) {
+		try {
+			Integer.parseInt(str);
+		} catch(NumberFormatException nfe){
+			return false;
+		}
+		return true;
+	}
+	
+	private boolean testaReal(String str) {
+		try {
+			Float.parseFloat(str);
+		} catch(NumberFormatException nfe){
+			return false;
+		}
+		return true;
 	}
 }
