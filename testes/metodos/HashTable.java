d31e66aafcbd4bbc1190b06874a215aea083d0f1
Artur Santos Nascimento <arturxz@outlook.com.br>
2018-09-04 00:23:49 -0300

    Adicionado métodos de inserção, busca e retorno de entradas do hashtable. Adicionados testes desses métodos.

--- testes/metodos/HashTable.java
+++ testes/metodos/HashTable.java
@@ -4,8 +4,31 @@ import java.util.Hashtable;
 
 public class HashTable {
 	
-	public static Hashtable<String, String> ht;
+	public static Hashtable<String, String[]> ht = new Hashtable<String, String[]>();
 	
-
+	public static boolean incluiEntrada(String tipo, String id, String var) {
+		if(buscaEntrada(id)) {
+			return false;
+		} else {
+			ht.put(id, new String[]{tipo, var});
+			return true;
+		}
+	}
+	
+	public static String[] retornaEntrada(String id) {
+		if(buscaEntrada(id)) {
+			return new String[]{ht.get(id)[0], id, ht.get(id)[1]};
+		} else {
+			return null;
+		}
+	}
+	
+	public static boolean buscaEntrada(String entrada) {
+		if(ht.get(entrada) == null) {
+			return false;
+		} else {
+			return true;
+		}
+	}
 
 }
--- testes/testes/TesteHashTable.java
+++ testes/testes/TesteHashTable.java
@@ -1,5 +1,10 @@
 package testes;
 
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+
 import java.util.Hashtable;
 
 import org.junit.AfterClass;
@@ -7,25 +12,57 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class TesteHashTable {
-
-	public static Hashtable<String, String> ht;
+	public static Hashtable<String, String[]> ht;
 	
 	@BeforeClass
-	public void inicializacao() {
-		ht = new Hashtable<String, String>();
-		
-		ht.put("nome", "artur");
-		ht.put("idade", "24");
-		ht.put("tamanho", "1,23");
+	public static void inicializacao() {
+		ht = metodos.HashTable.ht;
+		ht.put("nome",		new String[]{"string", "artur"});
+		ht.put("idade",		new String[]{"inteiro", "23"});
+		ht.put("tamanho",	new String[]{"real", "1.88"});
 	}
 	
 	@AfterClass
-	public void encerramento() {
+	public static void encerramento() {
 		ht.clear();
 	}
 	
+	// #################### TESTES DE BUSCA #################### //
+	
 	@Test
-	public void testeValidoExistente() {
+	public void testeBuscaExistente() {
+		assertTrue(metodos.HashTable.buscaEntrada("nome"));
+	}
+	
+	@Test
+	public void testeBuscaInexistente() {
+		assertFalse(metodos.HashTable.buscaEntrada("endereco"));
+	}
+	
+	@Test(expected = NullPointerException.class)
+	public void testeBuscaStringNula() {
+		assertFalse(metodos.HashTable.buscaEntrada(null));
 	}
 
+	// #################### TESTES DE INSERCAO #################### //
+	@Test
+	public void testeInsercaoSucesso() {
+		assertTrue(metodos.HashTable.incluiEntrada("string", "cor", "pardo") );
+	}
+	
+	@Test
+	public void testeInsercaoErro() {
+		assertFalse(metodos.HashTable.incluiEntrada("string", "nome", "artur") );
+	}
+	
+	// #################### TESTES DE OBTENCAO #################### //
+	@Test
+	public void testeRetornoExistente() {
+		assertArrayEquals(new String[]{"real", "tamanho", "1.88"}, metodos.HashTable.retornaEntrada("tamanho"));
+	}
+	
+	@Test
+	public void testeRetornoInexistente() {
+		assertNull(metodos.HashTable.retornaEntrada("cabelo"));
+	}
 }
