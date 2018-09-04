8e4ca4cd699d7026d8534e563a4ee42693cead6f
Artur Santos Nascimento <arturxz@outlook.com.br>
2018-09-04 01:53:14 -0300

    declaracao de constantes funcionando

--- src/jah/interpret/Interpreter.java
+++ src/jah/interpret/Interpreter.java
@@ -5,27 +5,44 @@ import jah.analysis.*;
 import java.lang.System;
 
 public class Interpreter extends DepthFirstAdapter {
-	
+
 	// PRODUCOES PARA PROGRAMA GERAL
 	public void outASemTudoPgr(ASemTudoPgr node) {
-		System.out.println("Compilando programa: " + node.getId() +" sem declaracoes e comandos.");
+		System.out.println("Executando programa: " + node.getId() + "sem declaracoes e comandos.");
 	}
 
 	public void outAComDeclPgr(AComDeclPgr node) {
-		System.out.println("Compilando programa: " + node.getId() +" apenas com declaracoes");
+		System.out.println("Executando programa: " + node.getId() + "apenas com declaracoes");
 	}
 
 	public void outAComCommPgr(AComCommPgr node) {
-		System.out.println("Compilando programa: " + node.getId() +" apenas com comandos.");
+		System.out.println("Executando programa: " + node.getId() + "apenas com comandos.");
 	}
 
 	public void outAComTudoPgr(AComTudoPgr node) {
-		System.out.println("Compilando programa: " + node.getId() +" com declaracoes e comandos.");
+		System.out.println("Executando programa: " + node.getId() + "com declaracoes e comandos.");
 	}
-	
-	// DECLARACOES
-	public void caseAVariaveisDecl(AVariaveisDecl node) {
+
+	// DECLARACAO DE CONSTANTES
+	public void caseAConstanteDecl(AConstanteDecl node) {
+		String id = node.getId().toString();
+		String va = node.getValor().toString();
 		
-	} 
-	
+		if( InterpreterUtil.buscaEntrada(id) ) {
+			String msg = "Erro! variavel ja declarada!\nLinha:" +node.getId().getLine() +", Posicao:" +node.getId().getPos();
+			System.out.println(msg);
+		} else {
+			if( ARealValor.class.isAssignableFrom( node.getValor().getClass() ) ) {
+				InterpreterUtil.incluiEntrada("real", id, va);
+			} else if( AInteiroValor.class.isAssignableFrom( node.getValor().getClass() ) ) {
+				InterpreterUtil.incluiEntrada("inteiro", id, va);
+			} else if( AStringValor.class.isAssignableFrom( node.getValor().getClass() ) ) {
+				InterpreterUtil.incluiEntrada("string", id, va);
+			} else {
+				System.out.println("Tipo nao encontrado");
+			}
+		}
+
+	}
+
 }
--- src/jah/interpret/InterpreterUtil.java
+++ src/jah/interpret/InterpreterUtil.java
@@ -4,5 +4,65 @@ import java.util.Hashtable;
 
 public class InterpreterUtil {
 	private static Hashtable<String, String[]> ht = new Hashtable<String, String[]>();
+	/**
+	 * A ESTRUTURA DO HASHTABLE SEGUE AS SEGUINTES REGRAS: 
+	 * ht<String: id, {String: tipo, String: informacao}>
+	 * 
+	 * O primeiro item eh um objeto String que referencia o id da variavel
+	 * O segundo item eh um array de String, onde: 
+	 * 		A primeira entrada eh o tipo da variavel (inteiro, real, string)
+	 * 		A segunda entrada eh a variavel (42, 3.1416, "vamola") ja validados.
+	**/
+	
+	public static boolean incluiEntrada(String tipo, String id, String var) {
+		/** INCLUI ENTRADA NO HASHTABLE. SUBTENDE-SE QUE O VALOR JA FOI VALIDADO! **/
+		if (buscaEntrada(id)) {
+			return false;
+		} else {
+			ht.put(id, new String[] { tipo, var });
+			return true;
+		}
+	}
+
+	public static String[] retornaEntrada(String id) {
+		/**
+		 * O RETORNO EH UM ARRAY DE STRING NA FORMA {TIPO, ID, INFORMACAO}. RETORNA TRUE
+		 * CASO ENCONTRE E NULL SE NAO EXISTIR ENTRADA COM ID BUSCADA
+		 **/
+		if (buscaEntrada(id)) {
+			return new String[] { ht.get(id)[0], id, ht.get(id)[1] };
+		} else {
+			return null;
+		}
+	}
+
+	public static boolean buscaEntrada(String entrada) {
+		/** APENAS VERIFICA SE ENTRADA EXISTE NO HASHTABLE A PARTIR DA ID **/
+		if (ht.get(entrada) == null) {
+			return false;
+		} else {
+			return true;
+		}
+	}
+
+	/** METODOS DE VALIDACAO **/
+	public static boolean validaReal(String str) {
+		str = str.replaceAll(",", ".");
+		try {
+			Float.parseFloat(str);
+		} catch (NumberFormatException nfe) {
+			return false;
+		}
+		return true;
+	}
+
+	public static boolean validaInteiro(String str) {
+		try {
+			Integer.parseInt(str);
+		} catch (NumberFormatException nfe) {
+			return false;
+		}
+		return true;
+	}
 
 }
